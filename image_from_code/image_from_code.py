caa688cf3200bf1cc0065d7d77743300310457de
arielnlm <mjovanovic3719rn@raf.rs>
2023-12-21 14:33:45 +0100

    comments

--- image_from_code/image_from_code.py
+++ image_from_code/image_from_code.py
@@ -18,7 +18,6 @@ def generisi_sliku_koda():
 
     kod = highlight(izvorni_kod, lexer, ImageFormatter(font_size=16, line_numbers=True))
     
-    # Dodajte dijalog za izbor direktorijuma i naziva slike
     file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
     
     if file_path:
@@ -34,31 +33,25 @@ def odaberi_fajl():
         text.delete("1.0", "end")
         text.insert("1.0", izvorni_kod)
 
-# Kreiranje glavnog prozora
 prozor = Tk()
 prozor.title("Generate photo from code")
 
-# Kreiranje tekstualnog polja za unos koda
 text = Text(prozor, width=40, height=10)
 text.pack()
 
-# Kreiranje padajućeg menija za izbor jezika
 jezik_var = StringVar()
-jezik_var.set("python")  # Postavite željeni podrazumevani jezik
+jezik_var.set("python")  
 jezik_label = Label(prozor, text="Choose language:")
 jezik_label.pack()
 jezik_menu = OptionMenu(prozor, jezik_var, "python", "c", "java", "javascript")
 jezik_menu.pack()
 
-# Dugme za generisanje slike
 generisi_dugme = Button(prozor, text="Generate photo", command=generisi_sliku_koda)
 generisi_dugme.pack()
 
-# Dugme za odabir fajla
 odaberi_dugme = Button(prozor, text="Choose file with code", command=odaberi_fajl)
 odaberi_dugme.pack()
 
-# Rezultat generisanja slike
 rezultat = Label(prozor, text="", fg="green")
 rezultat.pack()
 
